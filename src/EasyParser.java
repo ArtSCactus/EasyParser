e8294aa3db38f1fdf9e361b1a50e0e07f902a215
Artsiom (ArtSCactus) <artemir14@gmail.com>
2019-07-20 13:50:53 +0300

    Added new feature + test file
    Added possibility to read files.
    Added test file

--- src/EasyParser.java
+++ src/EasyParser.java
@@ -1,21 +1,39 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class EasyParser {
-private FileWriter writeThread;
-private FileReader readThread;
-private BufferedReader scan;
-private String filePath;
-public EasyParser(String filePath) throws IOException {
-    this.filePath=filePath;
-    writeThread= new FileWriter(filePath);
-    readThread = new FileReader(filePath);
-    scan = new BufferedReader(readThread);
-}
+    private FileWriter writeThread;
+    private FileReader readThread;
+    private BufferedReader scan;
+    private String filePath;
+
+    public EasyParser(String filePath) throws IOException {
+        this.filePath = filePath;
+        readThread = new FileReader(filePath);
+        scan = new BufferedReader(readThread);
+    }
+
+    public EasyParser (File file) throws IOException{
+        filePath=file.toString();
+        readThread= new FileReader(file);
+        scan = new BufferedReader(readThread);
+    }
+
+    public List<String> getRowsInList() throws IOException {
+        List<String> rows = new ArrayList<String>();
+        while (scan.ready())
+               rows.add(scan.readLine());
+               return rows;
+    }
     public static void main(String[] args) throws Exception {
+         EasyParser parser = new EasyParser("src/test.txt");
+         List<String> rows=parser.getRowsInList();
+         System.out.println(rows.toString());
 
     }
 
